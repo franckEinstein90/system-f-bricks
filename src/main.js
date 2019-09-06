bf6352d4dbc3a353a338416c2741b450b144cdbf
U-APCA2\Franck Binard <Franck Binard@BNO-L-PC0ANN8B.APCA2.GC.CA>
2019-09-06 15:59:01 -0400

    ..

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ jQuery(function($){
 	let $workspace = $("#workspace")
 	$('.drag')
 		.drag("start",function( ev, dd ){
-			dd.limit = $workspace.offset();
-			dd.limit.bottom = dd.limit.top + $workspace.outerHeight() - $( this ).outerHeight();
-			dd.limit.right = dd.limit.left + $workspace.outerWidth() - $( this ).outerWidth();
+			$( this ).addClass('active')
+			dd.limit = $workspace.offset()
+			dd.limit.bottom = dd.limit.top + $workspace.outerHeight() - $( this ).outerHeight()
+			dd.limit.right = dd.limit.left + $workspace.outerWidth() - $( this ).outerWidth()
 		})
+
 		.drag(function( ev, dd ){
 			$( this ).css({
 				top: Math.min( dd.limit.bottom, Math.max( dd.limit.top, dd.offsetY ) ),
 				left: Math.min( dd.limit.right, Math.max( dd.limit.left, dd.offsetX ) )
-			});   
-		});	
+			})
+		})
+
+		.drag('end', function(){
+			$( this ).removeClass('active')
+		})
 });
 
 
